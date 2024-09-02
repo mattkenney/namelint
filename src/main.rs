a3e1869fc3cf1b39f5d9b7733f614bd4e990024f
Andrew Marcuse <fileformat@gmail.com>
2024-09-02 13:41:06 -0400

    UPDATE: check for MS-DOS reserved names

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,33 @@ use std::fs::{self};
 use std::path::Path;
 use std::collections::VecDeque;
 use std::ffi::OsString;
-use clap::{arg, Command};
+use clap::{arg, Arg, ArgAction, Command};
+use regex::RegexBuilder;
+
+//struct for configuration
+struct Config {
+    allow_msdos: bool,
+}
 
 fn main() {
     let matches = Command::new("namelint")
         .version("1.0")
         .about("lint your file names")
+        .arg(Arg::new("msdos")
+            .action(ArgAction::SetTrue)
+            .default_value("false")
+            .help("Allow MS-DOS reserved file names (LPT1, COM1, etc.)")
+            .long("allow-msdos")
+            .value_parser(clap::builder::BoolishValueParser::new())
+        )
         .arg(arg!(<path>... "paths to check")
             .help("Path(s) to checks")
             .trailing_var_arg(true))
         .get_matches();
+
+    let config = Config {
+        allow_msdos: *matches.get_one::<bool>("msdos").unwrap(),
+    };
 
     let path_args: Vec<_> = matches.get_many::<String>("path")
         .expect("at least one path is required")
@@ -25,27 +42,46 @@ fn main() {
     }
     while path_queue.len() > 0 {
         let next_path = path_queue.pop_front().unwrap();
-        let new_paths = visit_dirs(Path::new(&next_path)).unwrap_or_else(|e| panic!("Unable to visit directory '{}': {}", next_path.to_string_lossy(), e));
+        let new_paths = visit_dirs(&config, &next_path).unwrap_or_else(|e| panic!("Unable to visit directory '{}': {}", next_path.to_string_lossy(), e));
         for new_path in new_paths.iter() {
             path_queue.push_back(new_path.clone());
         }
     }
 }
 
+/* check a file name for MS-DOS reserved names */
+fn check_msdos(name: &str) -> bool {
+    let msdos_reserved_pattern = RegexBuilder::new(r"(^|/)(CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])($|/|[.])")
+        .case_insensitive(true)
+        .build().unwrap();
+    msdos_reserved_pattern.is_match(name)
+}
+
 /* check all files in a directory, and return a list (possibly empty) of subdirectories */
-fn visit_dirs(dir: &Path) -> io::Result< Vec<OsString> > {
+fn visit_dirs(config: &Config, dir: &OsString) -> io::Result< Vec<OsString> > {
+
+    let dir_str = std::str::from_utf8(dir.as_encoded_bytes());
+    if dir_str.is_err() {
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Path is not UTF-8"));
+    }
+    let dir_str = dir_str.unwrap();
+    if (config.allow_msdos == false) && check_msdos(dir_str) {
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "MS-DOS reserved name"));
+    }
+
     let mut new_dirs:Vec::<OsString> = Vec::new();
     println!("Processing {:?}", dir);
-    if dir.exists() == false {
+    let path = Path::new(dir_str);
+    if path.exists() == false {
         //println!("Path does not exist: {:?}", dir);
         return Err(io::Error::new(io::ErrorKind::NotFound, "Path does not exist"));
     }
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
             let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                new_dirs.push(entry.path().into_os_string());
+            let entry_path = entry.path();
+            if entry_path.is_dir() {
+                new_dirs.push(entry_path.into_os_string());
             } else {
                 println!("File: {:?}", &path);
             }
