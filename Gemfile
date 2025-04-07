3a88be74afe625d1417dd384962cc86c01410566
Andrew Marcuse <fileformat@gmail.com>
2025-04-07 11:31:17 -0400

    Support for running website via docker

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "jekyll", "= 3.10"
+
+gem "webrick", "~> 1.9"
+gem "kramdown-parser-gfm", "~> 1.1"
+gem "jekyll-redirect-from", "~> 0.16.0"
--- www-run.sh
+++ www-run.sh
@@ -7,9 +7,31 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-jekyll serve \
-    --drafts \
-    --source docs \
-    --trace \
-    --watch
+USE_DOCKER=${USE_DOCKER:-false}
 
+echo "INFO: checking for jekyll"
+if ! command -v jekyll &> /dev/null; then
+	echo "INFO: jekyll not found, using docker"
+	USE_DOCKER=true
+fi
+
+if [ "$USE_DOCKER" = true ]; then
+	echo "INFO: running jekyll in docker"
+	docker run --rm -it \
+		-v "$(pwd):/srv/jekyll" \
+		-p 4000:4000 \
+		-e JEKYLL_ENV=development \
+		jekyll/builder \
+		sh -c "bundle install && bundle exec jekyll serve --drafts --source docs --trace --watch --host=0.0.0.0"
+
+else
+	echo "INFO: running jekyll locally"
+	jekyll serve \
+		--drafts \
+		--source docs \
+		--trace \
+		--watch
+fi
+
+
+#		sh -c "bundle init && bundle add jekyll -v 3.10 && bundle add webrick kramdown-parser-gfm jekyll-redirect-from && bundle exec jekyll serve --drafts --source docs --trace --watch --host=0.0.0.0"
