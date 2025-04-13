8a46cd87989c4c4613f9120cef778bdc1d7096f6
Matt Kenney <mkenney@mjk-tech.com>
2025-04-13 18:49:00 -0400

    add: lint_id in lints + validate with ref

--- bin/_validate.sh
+++ bin/_validate.sh
@@ -12,6 +12,7 @@ REPO_DIR="$( cd "${SCRIPT_DIR}/.." && pwd )"
 
 SCHEMA_YAML=${1:-BAD}
 YAML_DIR=${2:-BAD}
+REF_SCHEMA_YAML=${3:-}
 if [ "${SCHEMA_YAML}" = "BAD" ]; then
 	echo "ERROR: schema file not specified"
 	echo "Usage: $0 <schema.yaml> <target_dir>"
@@ -29,6 +30,11 @@ if [ ! -f "${SCHEMA_YAML}" ]; then
 	exit 1
 fi
 
+if [ -n "${REF_SCHEMA_YAML}" ] && [ ! -f "${REF_SCHEMA_YAML}" ]; then
+	echo "ERROR: ref schema file not found: ${REF_SCHEMA_YAML}"
+	exit 1
+fi
+
 # check that ajv is installed
 if ! command -v ajv > /dev/null; then
 	echo "ERROR: ajv is not installed: try 'npm install -g ajv-cli'"
@@ -39,11 +45,20 @@ echo "INFO: validation starting at $(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 
 for YAML_FILE in "${YAML_DIR}"/*.yaml; do
 	echo "INFO: validating ${YAML_FILE}"
-	ajv validate \
-		--errors=text \
-		--spec=draft2020 \
-		-s "${SCHEMA_YAML}" \
-		-d "${YAML_FILE}"
+	if [ -z "${REF_SCHEMA_YAML}" ]; then
+		ajv validate \
+			--errors=text \
+			--spec=draft2020 \
+			-s "${SCHEMA_YAML}" \
+			-d "${YAML_FILE}"
+	else
+		ajv validate \
+			--errors=text \
+			--spec=draft2020 \
+			-s "${SCHEMA_YAML}" \
+			-d "${YAML_FILE}" \
+			-r "${REF_SCHEMA_YAML}"
+	fi
 done
 
 echo "INFO: validation complete at $(date -u +'%Y-%m-%dT%H:%M:%SZ')"
--- bin/validate-configs.sh
+++ bin/validate-configs.sh
@@ -11,6 +11,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR="$( cd "${SCRIPT_DIR}/.." && pwd )"
 
 SCHEMA_YAML="${REPO_DIR}/docs/namelint-config-schema.yaml"
+REF_SCHEMA_YAML="${REPO_DIR}/docs/namelint-rule-schema.yaml"
 YAML_DIR="${REPO_DIR}/config"
 
-"${SCRIPT_DIR}/_validate.sh" "${SCHEMA_YAML}" "${YAML_DIR}"
+"${SCRIPT_DIR}/_validate.sh" "${SCHEMA_YAML}" "${YAML_DIR}" "${REF_SCHEMA_YAML}"
