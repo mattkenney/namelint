874e3e878f9e9016600b8fdfc049326ff144db17
Andrew Marcuse <fileformat@gmail.com>
2025-04-15 14:08:30 -0400

    Rust working with updated schemas

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+#![allow(unused)]
 
 mod safe_name;
 mod load;
--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,13 @@
-use jsonschema::{self, Validator};
+use jsonschema::{self, Retrieve, Uri, ValidationError, Validator};
+use serde_json::Value;
+use std::{collections::HashMap, fmt};
+
 use crate::load::must_load_data;
-use std::fmt;
 
 const DEFAULT_RULE_SCHEMA: &'static str = include_str!("../docs/namelint-rule-schema.yaml");
 const DEFAULT_CONFIG_SCHEMA: &'static str = include_str!("../docs/namelint-config-schema.yaml");
 
+#[derive(PartialEq)]
 pub enum SchemaType {
 	Rule,
 	Config,
@@ -17,6 +20,29 @@ impl SchemaType {
 			SchemaType::Config => "config",
 		}
 	}
+	/*
+	pub fn from_str(s: &str) -> Option<Self> {
+		match s {
+			"rule" => Some(SchemaType::Rule),
+			"config" => Some(SchemaType::Config),
+			_ => None,
+		}
+	}
+
+	pub fn get_validator(&self) -> Validator {
+		match self {
+			SchemaType::Rule => must_load_validator(SchemaType::Rule),
+			SchemaType::Config => must_load_validator(SchemaType::Config),
+		}
+	}
+	*/
+
+	pub fn eq(&self, other: &SchemaType) -> bool {
+		matches!(
+			(self, other),
+			(SchemaType::Rule, SchemaType::Rule) | (SchemaType::Config, SchemaType::Config)
+		)
+	}
 }
 
 impl fmt::Display for SchemaType {
@@ -28,6 +54,33 @@ impl fmt::Display for SchemaType {
 	}
 }
 
+struct RefRetriever {
+	schemas: HashMap<String, Value>,
+}
+
+
+impl Retrieve for RefRetriever {
+	fn retrieve(&self, uri: &Uri<String>) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
+		self.schemas
+			.get(uri.as_str())
+			.cloned()
+			.ok_or_else(|| format!("ERROF ref$ schema not found: {uri}").into())
+	}
+}
+
+fn build_ref_retriever() -> RefRetriever {
+	let mut retriever = RefRetriever {
+		schemas: HashMap::new(),
+	};
+
+	retriever.schemas.insert(
+		"https://www.namelint.dev/namelint-rule-schema.json".to_string(),
+		must_load_data(DEFAULT_RULE_SCHEMA, "yaml", "ref"),
+	);
+
+	retriever
+}
+
 pub fn must_load_validator(schema_type: SchemaType) -> Validator {
 
 	let schema_str = match schema_type {
@@ -37,7 +90,18 @@ pub fn must_load_validator(schema_type: SchemaType) -> Validator {
 
 	let schema = must_load_data(&schema_str, "yaml", schema_type.as_str());
 
-	let validator = jsonschema::validator_for(&schema);
+	let validator: Result<Validator, ValidationError>;
+	if schema_type == SchemaType::Config {
+		// Add the rule schema to the config schema
+		//let rule_schema = must_load_data(DEFAULT_RULE_SCHEMA, "yaml", "built-in");
+		//let retriever = InMemoryRetriever{ schema: rule_schema };
+		validator = jsonschema::options()
+			.with_retriever(build_ref_retriever())
+			.build(&schema);
+	} else {
+		validator = jsonschema::validator_for(&schema);
+	}
+
 	if validator.is_err() {
 		println!("ERROR: unable to load schema validator '{}': {}", schema_type, validator.err().unwrap());
 		std::process::exit(1);
